c867744fbb91955e6b970ef67cd8a2f7a534e41a
Unknown <nassos.adamopoulos@gmail.com>
2017-08-25 16:45:31 +0200

    First attempt at parsing files for Rgs

---
+++ map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import argparse
+import os
+import math
+
+
+with open("phase-1_Rg_values.log", 'r') as input_file:
+    for numbers in input_file:
+        print (numbers)
+         
